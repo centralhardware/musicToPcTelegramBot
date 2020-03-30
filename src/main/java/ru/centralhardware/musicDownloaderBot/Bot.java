8582a057a58e6b137d155b95b4b780f2bd9a1456
alex-comp <aleksey-fedechkin@rambler.ru>
2020-03-30 16:18:52 +0700

    add socks5 proxy support

--- src/main/java/ru/centralhardware/musicDownloaderBot/Bot.java
+++ src/main/java/ru/centralhardware/musicDownloaderBot/Bot.java
@@ -1,7 +1,9 @@
 package ru.centralhardware.musicDownloaderBot;
 
 import lombok.extern.log4j.Log4j;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.ApiContext;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -17,13 +19,24 @@ public class Bot extends TelegramLongPollingBot {
 
     private final Youtube youtube;
 
+
+
     /**
      * run bot
      */
     public static void run(){
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
-            telegramBotsApi.registerBot(new Bot());
+            if (Config.isUseProxy){
+                log.info("use proxy socks5://"+Config.proxyHost+":"+Config.proxyPort);
+                DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+                botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
+                botOptions.setProxyPort(Config.proxyPort);
+                botOptions.setProxyHost(Config.proxyHost);
+                telegramBotsApi.registerBot(new Bot(botOptions));
+            } else {
+                telegramBotsApi.registerBot(new Bot());
+            }
             log.info("bot register");
         } catch (TelegramApiRequestException e) {
             log.fatal("bot register fail", e);
@@ -34,6 +47,11 @@ public class Bot extends TelegramLongPollingBot {
      * init youtube-dl
      */
     public Bot() {
+        this.youtube = new Youtube(this);
+    }
+
+    public Bot(DefaultBotOptions defaultBotOptions){
+        super(defaultBotOptions);
         this.youtube = new Youtube(this);
     }
 
--- src/main/java/ru/centralhardware/musicDownloaderBot/Config.java
+++ src/main/java/ru/centralhardware/musicDownloaderBot/Config.java
@@ -9,5 +9,8 @@ public class Config {
     public static final String username = config.getString("username");
     public static final String token = config.getString("token");
     public static final boolean isTesting = Boolean.parseBoolean(config.getString("isTesting"));
+    public static final boolean isUseProxy = Boolean.parseBoolean(config.getString("isUseProxy"));
+    public static final String proxyHost = config.getString("proxyHost");
+    public static final int proxyPort = Integer.parseInt(config.getString("proxyPort"));
 
 }
